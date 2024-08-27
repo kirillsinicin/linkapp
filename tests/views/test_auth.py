d234ff17b7c3cd19e13f498444cacc4a45f3344a
Kirill Sinitsin <61236981+kirillsinicin@users.noreply.github.com>
2024-08-27 19:22:08 +0400

    update test

--- tests/views/test_auth.py
+++ tests/views/test_auth.py
@@ -1,10 +1,10 @@
 def test_register_ok(client):
-    response = client.post("/auth/register", data={"login": "hui", "password": "jopa"})
+    response = client.post("/auth/register", data={"login": "test_login", "password": "test_password"})
     assert response.status_code == 200
     assert type(response.json) is str
     assert response.json != ""
 
 
 def test_register_bad(client):
-    response = client.post("/auth/register", data={"login": "hui", "password": ""})
+    response = client.post("/auth/register", data={"login": "test_login", "password": ""})
     assert response.status_code == 401
